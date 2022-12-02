1bd8bdb89e6e30ea63d0617c6374d399113f8b05
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2022-12-02 13:14:33 +0500

    feat: добавить интерактив JavaScript

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+const profileEdit = document.querySelector('.profile__edit');
+let popup = document.querySelector('.popup');
+const popupClose = popup.querySelector('.popup__close');
+
+profileEdit.addEventListener('click', function() {
+  popup.classList.add('popup_opened');
+});
+
+popupClose.addEventListener('click', function() {
+  popup.classList.remove('popup_opened');
+});
+
+let profileName = document.querySelector('.profile__name');
+let profileProfession = document.querySelector('.profile__profession');
+let popupSave = document.querySelector('.popup__save');
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+
+    let popupName = document.querySelector('.popup__name');
+    let popupProfession = document.querySelector('.popup__profession');
+
+    profileName.textContent = popupName.value;
+    profileProfession.textContent = popupProfession.value;
+
+    popup.classList.remove('popup_opened');
+}
+
+popupSave.addEventListener('click', handleFormSubmit);
+
+
+
+
+
+
